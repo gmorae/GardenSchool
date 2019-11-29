ac486c2efd88b281f2253adf8c43ffda0ff4e01a
gmorae <gmmartins06@gmail.com>
2019-11-29 00:07:58 -0200

    components de cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CarousselComponent } from './components/caroussel/caroussel.component';
+import { CursosComponent } from './components/cursos/cursos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    CarousselComponent
+    CarousselComponent,
+    CursosComponent
   ],
   imports: [
     BrowserModule,
