905dab32344fc3ac171043ce286071946bb00dce
gmorae <gmmartins06@gmail.com>
2019-12-02 23:20:29 -0200

    Listagem dinamica na home

--- src/app/pages/adm/adm.component.ts
+++ src/app/pages/adm/adm.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ConsultaService } from 'src/app/service/consulta.service';
 
 @Component({
   selector: 'app-adm',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adm.component.css']
 })
 export class AdmComponent implements OnInit {
-
-  constructor() { }
+  public user: any;
+  public aluno: any;
+  public prof : any
+  public pegaRetornoAluno$: Subscription
+  constructor(private consulta: ConsultaService) { }
 
   ngOnInit() {
+    this.consulta.listaUser()
+    this.pegaRetornoAluno$ = this.consulta.pegaRetornoUser$.subscribe(() => {
+      this.user = this.consulta.User
+      this.aluno = this.consulta.filtro(this.user, 3).length
+      console.log(this.aluno);
+      this.prof = this.consulta.filtro(this.user, 2).length
+      
+    })
   }
 
 }
