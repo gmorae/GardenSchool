92f8bedd67d87063a8745b6bd50b8e946d6f30bd
gmorae <gmmartins06@gmail.com>
2019-12-02 23:17:02 -0200

    Listando professores

--- src/app/pages/adm/cadastrar-prof/cadastrar-prof.component.ts
+++ src/app/pages/adm/cadastrar-prof/cadastrar-prof.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ConsultaService } from 'src/app/service/consulta.service';
 
 @Component({
   selector: 'app-cadastrar-prof',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastrar-prof.component.css']
 })
 export class CadastrarProfComponent implements OnInit {
-
-  constructor() { }
+  public user: any;
+  public prof: any;
+  public pegaRetornoAluno$: Subscription
+  constructor(private consulta: ConsultaService) { }
 
   ngOnInit() {
+    this.consulta.listaUser()
+    this.pegaRetornoAluno$ = this.consulta.pegaRetornoUser$.subscribe(() => {
+      this.user = this.consulta.User
+      this.prof = this.consulta.filtro(this.user, 2)
+    })
   }
 
 }
