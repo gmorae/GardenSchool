4e6f66c0e582163b0cd925f5c5e792bd753bfbe6
gmorae <gmmartins06@gmail.com>
2019-12-04 23:45:41 -0200

    Validação de login nos demais usuarios

--- src/app/pages/login/auth.service.ts
+++ src/app/pages/login/auth.service.ts
@@ -12,15 +12,19 @@ export class AuthService {
   constructor(private rota: Router) { }
 
   fazerLogin(user: Login) {
-    if (user.email === 'a@a.com' && user.senha === '123') {
+    if (user.email === 'admin@teste.com' && user.senha === 'admin') {
       this.userAuth = true;
       this.rota.navigate(['/Adm'])
 
+    } else if (user.email === 'aluno@teste.com' && user.senha === 'aluno') {
+      this.userAuth = true;
+      this.rota.navigate(['/User'])
+    } else if (user.email === 'prof@teste.com' && user.senha === 'prof') {
+      this.userAuth = true;
+      this.rota.navigate(['/Prof'])
     } else {
       this.userAuth = false;
       alert('Usuario ou senha incorretos')
-
     }
-
   }
 }
